c2e11442c90ecc4abfc1cc69d195eac5f69ac1e3
Mateusz Dukowski <mateusz.dukowski@gmail.com>
2014-08-28 22:24:57 +0200

    FreeFieldFinder redesign.

--- src/main/java/model/addingElements/FreeFieldFinder.java
+++ src/main/java/model/addingElements/FreeFieldFinder.java
@@ -1,92 +1,54 @@
 package main.java.model.addingElements;
 
-import java.awt.Dimension;
 import java.awt.Point;
-import java.util.Iterator;
 
 import main.java.model.boards.BoardArea;
-import main.java.model.commandOperations.orientations.PointIteratorsCreator;
-import main.java.model.commandOperations.orientations.ToBottomIteratorImplementation;
-import main.java.model.commandOperations.orientations.ToRightIteratorImplementation;
 
 public class FreeFieldFinder {
 
 	private BoardArea area;
 
-	private PointIteratorsCreator verticalIteratorsCreator;
-	private PointIteratorsCreator horizontalIteratorsCreator;
-
-	private Point currentPoint;
-	private Iterator<Point> currentVerticalIterator;
-	private Iterator<Point> currentHorizontalIterator;
-
-	private static enum ProgressStatus {
-		MOVED_TO_NEXT, RESTARTED
-	};
-
 	public FreeFieldFinder(BoardArea area) {
-		this(area,
-				new PointIteratorsCreator(
-						new ToBottomIteratorImplementation(),
-						new Dimension(area.getRows(), area.getColumns())),
-				new PointIteratorsCreator(
-						new ToRightIteratorImplementation(),
-						new Dimension(area.getRows(), area.getColumns())));
-	}
-
-	public FreeFieldFinder(BoardArea area,
-			PointIteratorsCreator verticalIteratorsCreator,
-			PointIteratorsCreator horizontalIteratorsCreator) {
 		this.area = area;
-		this.verticalIteratorsCreator = verticalIteratorsCreator;
-		this.horizontalIteratorsCreator = horizontalIteratorsCreator;
 	}
 
 	public Point findFirstFreeField(final Point startPoint) {
-		currentPoint = startPoint;
-		currentVerticalIterator = verticalIteratorsCreator
-				.iterator(currentPoint);
-		currentHorizontalIterator = horizontalIteratorsCreator
-				.iterator(currentPoint);
+		final int columns = area.getColumns();
+		final int rows = area.getRows();
 
-		while (!isFieldFree(currentPoint)) {
-			if (advanceToNextPoint(currentVerticalIterator,
-					verticalIteratorsCreator) == ProgressStatus.MOVED_TO_NEXT)
-				continue;
-
-			if (advanceToNextPoint(currentHorizontalIterator,
-					horizontalIteratorsCreator) == ProgressStatus.MOVED_TO_NEXT)
-				continue;
-
-			if (isCycleDetected(startPoint))
+		int x = startPoint.x;
+		int y = startPoint.y;
+		
+		while(!isFieldFree(x, y))
+		{
+			y = circularAdd(y, rows);
+			if(y == 0)
+				x = circularAdd(x, columns);
+			if(isCycleDetected(startPoint, x, y))
 				return null;
 		}
-
-		return currentPoint;
+		
+		return new Point(x, y);
+	}
+	
+	private int circularAdd(int value, int maxValue)
+	{
+		value++;
+		if(value == maxValue)
+			value = 0;
+		return value;
 	}
 
-	private boolean isCycleDetected(final Point startPoint) {
-		return startPoint.equals(currentPoint) ? true : false;
-
+	private boolean isCycleDetected(final Point startPoint, int currentX, int currentY) {
+		if(startPoint.x != currentX)
+			return false;
+		if(startPoint.y != currentY)
+			return false;
+		
+		return true;
 	}
 
-	private ProgressStatus advanceToNextPoint(Iterator<Point> currentIterator,
-			PointIteratorsCreator iteratorsCreator) {
-		if (currentIterator.hasNext()) {
-			currentPoint = currentIterator.next();
-			return ProgressStatus.MOVED_TO_NEXT;
-		} else {
-			currentVerticalIterator = resetIterator(iteratorsCreator);
-			currentPoint = currentIterator.next();
-			return ProgressStatus.RESTARTED;
-		}
-	}
-
-	private Iterator<Point> resetIterator(PointIteratorsCreator creator) {
-		return creator.iterator(creator.firstElement(currentPoint));
-	}
-
-	private boolean isFieldFree(Point p) {
-		return area.getFieldValue(p.x, p.y) == 0;
+	private boolean isFieldFree(int x, int y) {
+		return area.getFieldValue(x, y) == 0;
 	}
 }
--- src/test/java/model/addingElements/FreeFieldFinderTest.java
+++ src/test/java/model/addingElements/FreeFieldFinderTest.java
@@ -1,17 +1,15 @@
 package test.java.model.addingElements;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
 import static test.java.model.boards.BoardAreaTestUtils.configureBoardAreaMock;
-import static test.java.model.boards.BoardAreaTestUtils.createPointsForEachElementInRow;
-import static test.java.model.boards.BoardAreaTestUtils.mockIterator;
 
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 import main.java.model.addingElements.FreeFieldFinder;
 import main.java.model.boards.BoardArea;
-import main.java.model.commandOperations.orientations.PointIteratorsCreator;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,58 +19,55 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import test.java.model.boards.FixedValue;
 
-import static test.java.model.boards.BoardAreaTestUtils.*;
-
 @RunWith(MockitoJUnitRunner.class)
 public class FreeFieldFinderTest {
-	
+
 	private static final int ROWS = 5;
 	private static final int COLUMNS = 5;
-	
+
 	private static final int FIRST_ROW = 0;
 	private static final int FIRST_COLUMN = 0;
 	private static final int LAST_ROW = ROWS - 1;
-	private static final int LAST_COLUMN = COLUMNS -1;
-	
-	private static final Point TOP_LEFT_POINT = new Point(FIRST_ROW, FIRST_COLUMN);
-	private static final Point BOTTOM_RIGHT_POINT = new Point(LAST_ROW, LAST_COLUMN);
-	
-	private static final int VAULE_RESERVED = 2; 
-	
+	private static final int LAST_COLUMN = COLUMNS - 1;
+
+	private static final Point TOP_LEFT_POINT = new Point(FIRST_ROW,
+			FIRST_COLUMN);
+	private static final Point BOTTOM_RIGHT_POINT = new Point(LAST_ROW,
+			LAST_COLUMN);
+
+	private static final int VAULE_RESERVED = 2;
+
 	@Mock
 	private BoardArea area;
 
-	private PointIteratorsCreator verticalIteratorsCreator = mockIteratorCreator(createPointsForEachElementInColumn(0, FIRST_ROW, 5));
-	private PointIteratorsCreator horizontalIteratorsCreator = mockIteratorCreator(createPointsForEachElementInRow(0, FIRST_COLUMN, 5));
-	
 	@InjectMocks
 	private FreeFieldFinder testObject;
 
 	@Test
 	public void findFirstFreeField_startPoint0x0AndEmptyArea_returnsPoint0x0() {
 		configureBoardAreaMock(area, ROWS, COLUMNS);
-		
+
 		Point result = testObject.findFirstFreeField(TOP_LEFT_POINT);
 		assertEquals(result, TOP_LEFT_POINT);
 	}
-	
+
 	@Test
 	public void findFirstFreeField_startPoint4x4AndEmptyArea_returnsPoint4x4() {
 		configureBoardAreaMock(area, ROWS, COLUMNS);
-		
+
 		Point result = testObject.findFirstFreeField(BOTTOM_RIGHT_POINT);
 		assertEquals(result, BOTTOM_RIGHT_POINT);
 	}
-	
+
 	@Test
 	public void findFirstFreeField_startPoint0x0AndPoint0x0Reserved_returnsPoint0x1() {
-		FixedValue[] fixedValues = {new FixedValue(0, 0, VAULE_RESERVED)};
+		FixedValue[] fixedValues = { new FixedValue(0, 0, VAULE_RESERVED) };
 		configureBoardAreaMock(area, ROWS, COLUMNS, fixedValues);
-		
+
 		Point result = testObject.findFirstFreeField(TOP_LEFT_POINT);
 		assertEquals(result, new Point(TOP_LEFT_POINT.x, TOP_LEFT_POINT.y + 1));
 	}
-	
+
 	@Test
 	public void findFirstFreeField_startPoint0x0AndPointsInFirstColumnReserved_returnsPoint0x1() {
 		FixedValue[] fixedValues = {
@@ -80,11 +75,29 @@ public class FreeFieldFinderTest {
 				new FixedValue(0, 1, VAULE_RESERVED),
 				new FixedValue(0, 2, VAULE_RESERVED),
 				new FixedValue(0, 3, VAULE_RESERVED),
-				new FixedValue(0, 4, VAULE_RESERVED)};
+				new FixedValue(0, 4, VAULE_RESERVED) };
 		configureBoardAreaMock(area, ROWS, COLUMNS, fixedValues);
-		
+
 		Point result = testObject.findFirstFreeField(TOP_LEFT_POINT);
 		assertEquals(result, new Point(TOP_LEFT_POINT.x + 1, TOP_LEFT_POINT.y));
+	}
+
+	@Test
+	public void findFirstFreeField_startPoint0x0AndnoEmptyFields_returnsNull() {
+		configureBoardAreaMock(area, ROWS, COLUMNS);
+		when(area.getFieldValue(anyInt(), anyInt())).thenReturn(VAULE_RESERVED);
+
+		Point result = testObject.findFirstFreeField(TOP_LEFT_POINT);
+		assertTrue(result == null);
+	}
+
+	@Test
+	public void findFirstFreeField_startPoint4x4AndPoint4x4IsReserved_returnsPoint0x0() {
+		configureBoardAreaMock(area, ROWS, COLUMNS, new FixedValue(
+				BOTTOM_RIGHT_POINT.x, BOTTOM_RIGHT_POINT.y, VAULE_RESERVED));
+
+		Point result = testObject.findFirstFreeField(BOTTOM_RIGHT_POINT);
+		assertEquals(result, TOP_LEFT_POINT);
 	}
 
 }
