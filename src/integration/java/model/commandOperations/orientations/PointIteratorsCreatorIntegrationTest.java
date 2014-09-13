a34f28228ddbdb637b31b22d5f28caa972dfe24f
Dukos <mateusz.dukowski@gmail.com>
2014-09-13 06:56:16 +0200

    Moved integration test.

--- src/integration/java/model/commandOperations/orientations/PointIteratorsCreatorIntegrationTest.java
+++ src/integration/java/model/commandOperations/orientations/PointIteratorsCreatorIntegrationTest.java
@@ -1,4 +1,4 @@
-package test.java.model.commandOperations.orientations;
+package integration.java.model.commandOperations.orientations;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
